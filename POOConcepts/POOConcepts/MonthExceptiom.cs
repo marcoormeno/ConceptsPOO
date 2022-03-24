251eafc128cf67afdadab8d8bbb10562ac5ca13b
Marco Antonio Ormeño Martinez <marco_ormeno@hotmail.com>
2022-03-24 14:14:56 -0500

    Finish date class MAOM

---
+++ POOConcepts/POOConcepts/MonthExceptiom.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace POOConcepts
+{
+    [Serializable]
+    internal class MonthExceptiom : Exception
+    {
+        public MonthExceptiom()
+        {
+        }
+
+        public MonthExceptiom(string? message) : base(message)
+        {
+        }
+
+        public MonthExceptiom(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected MonthExceptiom(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1979, 1, 11));
+    Console.WriteLine(new Date(1984, 11, 31));
+    Console.WriteLine(new Date(2011, 6, 3));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine (error.Message );
+}
+
+
+
+
+
